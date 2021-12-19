32f00441de24e31f6139f58e81ef28d75b153acf
yaroslav.dubanovskyi <yaroslav.dubanovskyi@gmail.com>
2021-12-19 22:21:11 +0200

    Added UI client

---
+++ WebTitleParserTest/TestTitleCrawlerClient/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTitleCrawlerClient.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string Urls { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task OnPost()
+        {
+            var urls = Urls?.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            if (urls != null && urls.Length > 0)
+            {
+                var json = JsonConvert.SerializeObject(urls);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                using HttpClient client = new HttpClient();
+                var response = await client.PostAsync("https://testtitlecrawler.azurewebsites.net/api/ToCrawl", content);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Urls = string.Empty;
+                }
+                ViewData["SubmitStatus"] = response.StatusCode;
+            }
+        }
+    }
+}
---
+++ WebTitleParserTest/TestTitleCrawlerClient/Pages/Results.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace TestTitleCrawlerClient.Pages
+{
+    public class TitleResponce
+    {
+        [JsonProperty("titles")]
+        public string[] Titles { get; set; }
+        [JsonProperty("domain")]
+        public string Domain { get; set; }
+    }
+
+    public class ResultsModel : PageModel
+    {
+        private readonly ILogger<ResultsModel> _logger;
+
+        public List<TitleResponce> TitlesResponse { get; set; }
+
+        public ResultsModel(ILogger<ResultsModel> logger)
+        {
+            _logger = logger;
+        }
+        public async Task OnGet()
+        {
+            using HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://testtitlecrawler.azurewebsites.net/api/Crawled");
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var pageContents = await response.Content.ReadAsStringAsync();
+                var titles = JsonConvert.DeserializeObject<List<TitleResponce>>(pageContents);
+                TitlesResponse = titles;
+            }
+        }
+    }
+}
